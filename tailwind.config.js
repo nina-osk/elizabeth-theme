bf2b418903af68b73b4608418e796930191c57cd
nina-osk <ing.oscarina@gmail.com>
2026-01-18 13:02:42 +0100

    Update the dropdown boton

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './layout/**/*.{liquid,html}',
+    './sections/**/*.{liquid,html}',
+    './snippets/**/*.{liquid,html}',
+    './templates/**/*.{liquid,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
